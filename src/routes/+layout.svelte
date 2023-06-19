5df325b06144a067ff2a6529730fdef17c94026f
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2023-06-19 21:20:48 +0700

    seperated FRONTEND

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from './Header.svelte';
+	import Home from './+page.svelte';
+	let mode = 'Home';
+</script>
+
+<div class="app">
+	<Header bind:mode />
+
+	<main>
+		<Home bind:mode />
+	</main>
+</div>
+
+<style>
+</style>
